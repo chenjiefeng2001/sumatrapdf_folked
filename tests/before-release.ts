0905ebc05325340df4ee1f8cc4f606f6c09424d4
chenjiefeng <1497735430@qq.com>
2026-08-13 13:21:07 +0800

    tests: register ad-hoc-notif-elastic in before-release suite
    prompt: 请分批提交当前应该提交未追踪的文件

--- tests/before-release.ts
+++ tests/before-release.ts
@@ -13,6 +13,7 @@ import { testit as adHocTripleClickLine } from "./ad-hoc-triple-click-line.ts";
 import { testit as adHocTocPaletteSync } from "./ad-hoc-toc-palette-sync.ts";
 import { testit as adHocJpegDecode } from "./ad-hoc-jpeg-decode.ts";
 import { testit as adHocPasteImageAnnot } from "./ad-hoc-paste-image-annot.ts";
+import { testit as adHocNotifElastic } from "./ad-hoc-notif-elastic.ts";
 import { testit as latexTests } from "./latex.ts";
 import { testit as issueChmLzx } from "./issue-chm-lzx.ts";
 
@@ -23,6 +24,7 @@ const adHocTests: [string, () => void | Promise<void>][] = [
   ["ad-hoc-toc-palette-sync", adHocTocPaletteSync],
   ["ad-hoc-jpeg-decode", adHocJpegDecode],
   ["ad-hoc-paste-image-annot", adHocPasteImageAnnot],
+  ["ad-hoc-notif-elastic", adHocNotifElastic],
   ["issue-chm-lzx", issueChmLzx],
 ];
 
